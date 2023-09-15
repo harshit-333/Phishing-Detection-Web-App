700bb815f0de3bd47dddb2e251289ac89c443a6e
harshit-333 <harshit3.0titan@gmail.com>
2023-09-15 10:24:39 +0530

    final commit

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 
 from flask import Flask, request, render_template
 import numpy as np
-import pandas as pd
-from sklearn import metrics 
 import warnings
 import pickle
 warnings.filterwarnings('ignore')
